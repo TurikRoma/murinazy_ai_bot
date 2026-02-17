3bc759bf8cd85c3e0d47f5c46e0f8fd837375446
TurikRoma <turikroma15@gmail.com>
2026-02-17 18:24:18 +0300

    fa

--- bot/schemas/exercise.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from database.models import EquipmentTypeEnum
-
-
-class ExerciseCreate(BaseModel):
-    name: str
-    muscle_groups: str
-    equipment_type: EquipmentTypeEnum
--- bot/states/workout.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class WorkoutState(StatesGroup):
-    in_progress = State()
-    waiting_for_feedback = State()
-    waiting_for_rating = State()
